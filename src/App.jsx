70445106293b15cd9299adf2728b5a2662858dfa
Olesia Shcherbenko <oshch96@gmail.com>
2024-03-06 09:05:29 +0100

    My first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+import './App.scss'
+import image from './assets/image-omelette.jpeg';
+import Preparation from './components/Preparation';
+import Ingredients from './components/Ingredients';
+import Instructions from './components/Instructions';
+
+function App() {
+  
+  return (
+    <div className='card'>
+      <img src={image} className='img' alt=''/>
+      <div className='content'>
+        <div>
+          <h1>Simple Omelette Recipe</h1>
+          <p>An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats.</p>
+        </div>
+        <Preparation />
+        <Ingredients />
+        <hr className='line'></hr>
+        <Instructions />
+      </div>
+      <p>Nutrition The table below shows nutritional values per serving without the additional fillings. Calories 277kcal Carbs 0g Protein 20g Fat 22g</p>
+    {/*
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Preparation.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Preparation.scss';
+
+function Preparation() {
+    return (
+        <div className="prepatation">
+            <h3>Preparation time </h3>
+            <ul>
+                <li><span>Total:</span> Approximately 10 minutes</li>
+                <li><span>Preparation:</span> 5 minutes</li>
+                <li><span>Cooking:</span> 5 minutes </li>
+            </ul>
+        </div>
+    )
+};
+
+export default Preparation;
